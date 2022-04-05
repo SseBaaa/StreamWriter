5e6afb19460631bf2118c688da6333413858d719
SseBaaa <89750702+SseBaaa@users.noreply.github.com>
2022-04-05 13:09:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace klasaStreamWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Upišite ime: ");
+            string ime = Console.ReadLine();
+            Console.WriteLine("Upišite prezime: ");
+            string prezime = Console.ReadLine();
+
+            StreamWriter sw = new StreamWriter(@"C:\prviDir\mojaDatoteka.txt");
+
+            sw.WriteLine("Ime: {0}", ime); 
+            sw.WriteLine("Prezime: {0}", prezime);
+            sw.Close();
+            */
+
+            StreamReader sr = new StreamReader(@"C:\prviDir\mojaDatoteka.txt");
+
+
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            sr.Close();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
